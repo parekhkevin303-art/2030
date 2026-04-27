1b21eed121f1e96e0981c679d1ccc3c818bad1f2
HP <HP@LAPTOP-2UF4709R>
2026-04-27 20:54:47 +0530

    Adding new Project

---
+++ 2030/src/main/java/Frame.java
@@ -0,0 +1,25 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Frame {
+
+	
+		// TODO Auto-generated method stub
+
+		public static void main(String[] args) throws Exception {
+			// Set the property of chrome browser and pass chromedriver path
+			
+			System.out.print("hello");
+			WebDriver driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			driver.get("https://paytm.com/");
+			
+			
+			
+	}
+
+}
